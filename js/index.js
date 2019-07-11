d856b92539f963ffcee4508fe80522bdfd394947
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 01:14:12 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 var element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
