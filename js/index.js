1488833fc07c14d5cf60cd891bbea63d411ff784
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 01:14:24 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
 document.body.appendChild(element);
 element.style.textAlign = 'center';
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
